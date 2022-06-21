274344d3e201f04bd4e7b2a4dfa72f0f97482dec
Adithyan M.S <adithyan2ms@gmail.com>
2022-06-22 00:46:08 +0530

    till admin gets details

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,6 +1,9 @@
 const userDetailModel = require("../models/userdetails")
 const AdminDetailModel = require("../models/admindetails")
 const { response } = require("express")
+const bcrypt = require('bcrypt')
+var ObjectId=require('mongodb').ObjectID
+
 
 module.exports={
     addDetails:(detail)=>{
@@ -18,46 +21,57 @@ module.exports={
         })
     },
     doLogin:(data)=>{
-        return new Promise((resolve,reject)=>{
-            let response = false
-            AdminDetailModel.findOne({email:data.Email}).then((response)=>{
-                response = true
-                resolve(response)
+        return new Promise(async(resolve,reject)=>{
+            let response={}
+            let user = await AdminDetailModel.findOne({email:data.Email})
+            console.log(user);
+                if(user){
+                    bcrypt.compare(data.Password,user.password).then((status)=>{
+                        if(status){
+                            console.log("login sucess");
+                            response.user=user
+                            response.status=true
+                            resolve(response)
+                        }
+                        else{
+                            console.log("login failed");
+                            resolve({status:false})
+                        }
+                    })
+                }else{
+                    console.log("login failed");
+                    resolve({status:false})
+    
+                }
             })
-            
-            // if(user)
-            // {
-            //     user.findOne({password:data.Password}).then((status)=>{
-            //         if(status){
-            //             console.log("login success");
-            //             response=true
-            //             resolve(response)
-            //         }
-            //         else
-            //         {
-            //             console.log("can't login");
-            //             response=false
-            //         }
-            //     })
-            // }
-            // else{
-            //     console.log("can't login");
-            //     response=false
-            // }
-        })
     },
     doSignup:(data)=>{
         return new Promise(async(resolve,reject)=>{
+            data.password=await bcrypt.hash(data.password,10)
             var AdminModel =  AdminDetailModel(data)
             await AdminModel.save((err,data)=>{
                 if(err){
                     console.error(err);
                 }
                 else{
-                    console.log("data added");
-                    response=true
-                    resolve(response)
+                    // console.log("data added");
+                    resolve(data)
+                    // response=true
+                    // resolve(response)
                 }
+            })
+        })
+    },
+    getAllDetails:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let details= await userDetailModel.find()
+            resolve(details)
+        })
+    },
+    getUserDetails:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            await userDetailModel.findOne({_id:ObjectId(userId)}).then((detail)=>{
+                resolve(detail)
             })
         })
     }
--- routes/admin.js
+++ routes/admin.js
@@ -10,18 +10,21 @@ router.get('/',(req,res)=>{
 router.post('/login',(req,res)=>{
     console.log(req.body.Email);
     helper.doLogin(req.body).then((response)=>{
-        if(response)
-        {
-            console.log("ogggggg");
-        }
-        else{
-            console.log("lknklnk");
-        }
+        if(response.status){
+            res.redirect('/admin/home')
+
+            // res.render('/home',{layout: 'layouts/adminLayout.ejs'})
+          }else{
+              console.log("invalid username or password");
+            res.redirect('/admin')
+          }
     })
 
 })
-router.get('/home',(req,res)=>{
-    res.render('admin/home',{layout: 'layouts/adminLayout.ejs'})
+router.get('/home',async(req,res)=>{
+    await helper.getAllDetails().then((details)=>{
+        res.render('admin/home',{layout: 'layouts/adminLayout.ejs',details})
+    })
 })
 router.get('/signup',(req,res)=>{
     res.render('admin/signup',{layout: 'layouts/adminLayout.ejs'})
@@ -32,11 +35,20 @@ router.post('/signup',(req,res)=>{
         if(response)
         {
             console.log("added");
+            res.redirect('/admin')
+            // res.render('admin/login',{layout: 'layouts/adminLayout.ejs'})
         }
         else{
             console.log("error");
         }   
     })
+})
+router.get('/home/view-details/:id',async(req,res)=>{
+    var detail = await helper.getUserDetails(req.params.id)
+        console.log(detail);
+        res.render('admin/view-details',{layout: 'layouts/adminLayout.ejs',detail})
+
+    // console.log(req.params.id);
 })
 
 
